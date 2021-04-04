31e5ba7e65949f141b57220d5bfdb11ed55b70e5
Hamza Ibrahim <hamze.85@live.com>
2021-04-04 10:24:34 +0300

    Route Structure & Route Service Provider

---
+++ starter/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test1', function () {
+    return view('welcome');
+});
+
+Route::get('/test2/{id}', function ($id) {
+    return $id;
+})->name('a');
+
+
+Route::get('/test3/{id?}', function () {
+    return view('welcome');
+})->name('b');
+
